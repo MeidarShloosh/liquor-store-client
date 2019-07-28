15011fbd324e0273f99aba57ef3b4b3b854e0d39
Meidar <meidarshloosh@gmail.com>
2019-07-28 11:20:13 +0300

    added Snacks and Accessories pages

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,14 @@ import authReducer from './authReducer'
 import cartReducers from "./cartReducers";
 import storeReducer from "./storeReducer";
 import cocktailReducer from "./cocktailReducer";
+import accessoriesReducer from "./accessoriesReducer";
+import snacksReducer from "./snacksReducer";
 
 export default combineReducers({
     auth: authReducer,
     cart: cartReducers,
     store: storeReducer,
-    cocktails: cocktailReducer
+    cocktails: cocktailReducer,
+    accessories: accessoriesReducer,
+    snacks: snacksReducer
 })
